6d4903a591be48d9bf71dce277df9957203e12cf
Azeem Ghumman <azeemghumman3@gmail.com>
2019-04-29 03:49:38 +0000

    robot travelling in straigt line working

---
+++ Project2/artifacts/files/build/ball_chaser/CMakeFiles/ball_chaser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ball_chaser.dir/src/ball_chaser.cpp.o"
+  "/home/workspace/catkin_ws/devel/lib/ball_chaser/ball_chaser.pdb"
+  "/home/workspace/catkin_ws/devel/lib/ball_chaser/ball_chaser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ball_chaser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
